467f5776af0f2ed834f7a03ebae6aa7319bcbef6
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-01 12:19:07 -0500

    display user

---
+++ script.js
@@ -0,0 +1,17 @@
+const baseEndpoint = 'https://api.github.com';
+const usersEndpoint = `${baseEndpoint}/users`;
+const userEl = document.querySelector('.user');
+
+async function displayUser(username){
+    userEl.textContent = '⏳ loading....';
+    const res = await fetch(`${usersEndpoint}/${username}`);
+    const data = await res.json();
+    console.log(data);
+    console.log(data.html_url);
+    console.log(data.name);
+    console.log(data.location);
+    userEl.textContent = `${data.name} - ${data.html_url}`;
+
+};
+
+displayUser('codiemaureen');
